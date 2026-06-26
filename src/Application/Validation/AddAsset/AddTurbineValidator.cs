e78c10fbef0612a05018ceb00b27196eba72513d
rezatajari <reza.tajari70@gmail.com>
2026-06-26 18:44:07 +0330

    feat: AddTurbineValidator

---
+++ src/Application/Validation/AddAsset/AddTurbineValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs.AddAsset;
+using FluentValidation;
+
+namespace Application.Validation.AddAsset;
+
+public sealed class AddTurbineValidator : AbstractValidator<AddTurbine>
+{
+    public AddTurbineValidator()
+    {
+        RuleFor(x => x.WindSpeed)
+            .GreaterThanOrEqualTo(0);
+
+        RuleFor(x => x.SetPoint)
+            .InclusiveBetween(0, 100);
+
+        RuleFor(x => x.ActivePower)
+            .GreaterThanOrEqualTo(0);
+    }
+}
