12500265aaf6a018c5559e93906637d4a250e12d
rezatajari <reza.tajari70@gmail.com>
2026-06-26 18:42:53 +0330

    feat: AddBatteryValidator

--- src/Application/DTOs/AssetMonitoring/UserMonitoringInfoDto.cs
+++ src/Application/DTOs/AssetMonitoring/UserMonitoringInfoDto.cs
@@ -1,11 +1,4 @@
-﻿using Domain.Entities.Assets;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.DTOs.AssetMonitoring;
+﻿namespace Application.DTOs.AssetMonitoring;
 
 public record UserMonitoringInfoDto(
     string? Username,
---
+++ src/Application/Validation/AddAsset/AddBatteryValidator.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs.AddAsset;
+using FluentValidation;
+
+namespace Application.Validation.AddAsset;
+
+public sealed class AddBatteryValidator : AbstractValidator<AddBattery>
+{
+    public AddBatteryValidator()
+    {
+        RuleFor(x => x.Capacity)
+            .GreaterThan(0);
+
+        RuleFor(x => x.StateCharge)
+            .InclusiveBetween(0, 100);
+
+        RuleFor(x => x.SetPoint)
+            .InclusiveBetween(0, 100);
+
+        RuleFor(x => x.FrequentlyDischarge)
+            .InclusiveBetween(0, 100);
+    }
+}
