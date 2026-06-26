c077d5a9b8d7db8cb50db95fec44bb6e2b54f795
rezatajari <reza.tajari70@gmail.com>
2026-06-26 18:55:07 +0330

    refactor: some remove methods in assetservice

--- src/Application/Implementations/Asset/AssetService.cs
+++ src/Application/Implementations/Asset/AssetService.cs
@@ -1,7 +1,6 @@
 ﻿using Application.Common;
 using Application.DTOs.AddAsset;
 using Application.Interfaces.Asset;
-using Application.Interfaces.User;
 using Domain.Entities.Assets;
 
 namespace Application.Implementations.Asset
@@ -9,8 +8,7 @@ namespace Application.Implementations.Asset
     public class AssetService(
         IAssetRepository assetRepository,
         ISiteService siteService,
-        IUnitOfWork unitOfWork,
-        IUsersService userService)
+        IUnitOfWork unitOfWork)
         : IAssetService
     {
         public async Task<GeneralResponse<Guid>> AddBattery(Guid userId, AddBattery addBattery, Guid siteId)
@@ -70,48 +68,6 @@ namespace Application.Implementations.Asset
             return GeneralResponse<Guid>.Success(
                 turbine.Id,
                 message: "Turbine added successfully");
-        }
-
-        public async Task<GeneralResponse<SolarPanel>> GetSolarByUserIdAsync(Guid userId)
-        {
-            var userValidation = userService.ValidateUserId(userId);
-            if (!userValidation.IsSuccess)
-                return GeneralResponse<SolarPanel>.Failure(message: userValidation.Message, errors: userValidation.Errors);
-
-            var solar = await assetRepository.GetSolarByUserId(userId);
-
-            return GeneralResponse<SolarPanel>.Success(
-                data:solar,
-                message:"Solar found is successfully"
-                );
-        }
-
-        public async Task<GeneralResponse<WindTurbine>> GetTurbineByUserIdAsync(Guid userId)
-        {
-            var userValidation = userService.ValidateUserId(userId);
-            if (!userValidation.IsSuccess)
-                return GeneralResponse<WindTurbine>.Failure(message: userValidation.Message, errors: userValidation.Errors);
-
-            var turbine = await assetRepository.GetTurbineByUserId(userId);
-
-            return GeneralResponse<WindTurbine>.Success(
-                data: turbine,
-                message: "Solar found is successfully"
-            );
-        }
-
-        public async Task<GeneralResponse<Battery>> GetBatteryByUserIdAsync(Guid userId)
-        {
-            var userValidation = userService.ValidateUserId(userId);
-            if (!userValidation.IsSuccess)
-                return GeneralResponse<Battery>.Failure(message: userValidation.Message, errors: userValidation.Errors);
-
-            var battery = await assetRepository.GetBatteryByUserId(userId);
-
-            return GeneralResponse<Battery>.Success(
-                data: battery,
-                message: "Solar found is successfully"
-            );
         }
     }
 }
--- src/Application/Interfaces/Asset/IAssetService.cs
+++ src/Application/Interfaces/Asset/IAssetService.cs
@@ -9,7 +9,4 @@ public interface IAssetService
     Task<GeneralResponse<Guid>> AddBattery(Guid userId, AddBattery addBattery,Guid siteId);
     Task<GeneralResponse<Guid>> AddSolar(Guid userId, AddSolar addSolar, Guid siteId);
     Task<GeneralResponse<Guid>> AddTurbine(Guid userId, AddTurbine addTurbine, Guid siteId);
-    Task<GeneralResponse<SolarPanel>> GetSolarByUserIdAsync(Guid userId);
-    Task<GeneralResponse<WindTurbine>> GetTurbineByUserIdAsync(Guid userId);
-    Task<GeneralResponse<Battery>> GetBatteryByUserIdAsync(Guid userId);
 }
