323de21e33f9fb4696bd40e9313cc8cf43b98600
rezatajari <reza.tajari70@gmail.com>
2026-06-26 18:43:48 +0330

    feat: AddSiteValidator

---
+++ src/Application/Validation/AddAsset/AddSiteValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.AddAsset;
+using FluentValidation;
+
+namespace Application.Validation.AddAsset;
+
+public sealed class AddSiteValidator : AbstractValidator<AddSite>
+{
+    public AddSiteValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(50);
+
+        RuleFor(x => x.Location)
+            .NotEmpty()
+            .MaximumLength(100);
+    }
+}
---
+++ src/Application/Validation/AddAsset/AddSolarValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs.AddAsset;
+using FluentValidation;
+
+namespace Application.Validation.AddAsset;
+
+public sealed class AddSolarValidator : AbstractValidator<AddSolar>
+{
+    public AddSolarValidator()
+    {
+        RuleFor(x => x.Irradiance)
+            .GreaterThanOrEqualTo(0);
+
+        RuleFor(x => x.SetPoint)
+            .InclusiveBetween(0, 100);
+
+        RuleFor(x => x.ActivePower)
+            .GreaterThanOrEqualTo(0);
+    }
+}
