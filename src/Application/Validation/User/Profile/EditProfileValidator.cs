a4886b15de0d06baf7c0a2016bb6a12ddb7fba15
rezatajari <reza.tajari70@gmail.com>
2026-06-26 18:41:30 +0330

    feat: EditProfileValidator

---
+++ src/Application/Validation/User/Profile/EditProfileValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs.User.Profile;
+using FluentValidation;
+
+namespace Application.Validation.User.Profile;
+
+public sealed class EditProfileValidator : AbstractValidator<EditProfile>
+{
+    public EditProfileValidator()
+    {
+        RuleFor(x => x.UserName)
+            .NotEmpty()
+            .MinimumLength(3)
+            .MaximumLength(50)
+            .Matches(@"^[a-zA-Z0-9_\-\.]+$");
+
+        RuleFor(x => x.ProfileImage)
+            .MaximumLength(2048)
+            .When(x => !string.IsNullOrWhiteSpace(x.ProfileImage));
+    }
+}
